0747992f659872d9abc98cbbe4803eb5b2db4c07
Anurag Lodhi <anuraglodhi63@gmail.com>
2023-06-10 11:12:50 +0530

    Hittable and sphere utility classes

---
+++ lib/sphere.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "hittable.hpp"
+#include "vec3.hpp"
+
+class sphere : public hittable {
+   public:
+    sphere() {}
+    sphere(point3 center, double radius) : center(center), radius(radius) {}
+
+    virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+   public:
+    point3 center;
+    double radius;
+};
+
+bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    vec3 oc = r.origin() - center;
+    double a = r.direction().length_squared();
+    double half_b = dot(oc, r.direction());
+    double c = oc.length_squared() - radius * radius;
+
+    double discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) return false;
+
+    double sqrtd = sqrt(discriminant);
+
+    // Nearest root in range
+    double root = (-half_b - sqrtd) / a;
+    if (root < t_min || t_max < root) {
+        root = (-half_b + sqrtd) / a;
+        if (root < t_min || t_max < root) {
+            return false;
+        }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    rec.normal = (rec.p - center) / radius;
+
+    return true;
+}
